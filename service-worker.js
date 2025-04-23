54da2ac8f059f53d28a3d9c682ed441167198975
Aron Joshua <aronjoshua.maqui@gmail.com>
2025-04-23 02:03:22 -0400

    Initial commit: Add PWA files

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'resolution-slider-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/assets/icon-512.png',
+  '/assets/icon-192.png',
+  '/assets/icon-144.png',
+  '/assets/icon-96.png',
+  '/assets/icon-72.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => {
+          return caches.match('/offline.html');
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
